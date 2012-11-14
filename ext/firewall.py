a2bbd83a94532cf9b593d4773f205cc3de4e9d26
Wonjohn Choi <wonjohn.choi@gmail.com>
2012-11-13 18:09:06 -0800

    Refactored

--- ext/firewall.py
+++ ext/firewall.py
@@ -9,25 +9,27 @@ log = core.getLogger("fw")
 
 class Firewall (object):
   def __init__(self):
+    self.debug = True
     self.banned_ports = set()
     self.banned_domains = set()
     self.monitor_strings = dict()
 
+    self.str_conn = lambda flow : "[" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]"
     self.prev_packet = dict()
     self.timers = dict()
+    self.counts_file = open('/root/pox/ext/counts.txt', 'w')
 
     with open('/root/pox/ext/banned-ports.txt', 'r') as f:
       for port in f:
-        self.banned_ports.add(int(port.rstrip()))
+        self.banned_ports.add(int(port.strip()))
 
     with open('/root/pox/ext/banned-domains.txt', 'r') as f:
       for domain in f:
-        self.banned_domains.add(str(domain.rstrip()))
+        self.banned_domains.add(str(domain.strip()))
 
     with open('/root/pox/ext/monitored-strings.txt', 'r') as f:
       for pair in f:
-        ip, string = pair.split(':')
-        string = string.rstrip()
+        ip, string = pair.strip().split(':')
         if ip not in self.monitor_strings.keys():
           self.monitor_strings[ip] = dict()
           self.prev_packet[ip] = dict()
@@ -35,8 +37,9 @@ class Firewall (object):
         self.monitor_strings[ip][string] = 0
         self.prev_packet[ip][string] = ""
    
-    log.debug("Firewall initialized.")
-
+    
+    if self.debug: log.debug("Firewall initialized.")
+    
   def _handle_ConnectionIn (self, event, flow, packet):
     """
     New connection event handler.
@@ -44,10 +47,10 @@ class Firewall (object):
     action property of the event.
     """
     if flow.dstport in self.banned_ports:
-      log.debug("Denied connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+      if self.debug: log.debug("Denied connection " + self.str_conn(flow))
       event.action.forward = False
     else:
-      log.debug("Deferred connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+      if self.debug: log.debug("Deferred connection " + self.str_conn(flow))
       event.action.defer = True
 
   def _handle_DeferredConnectionIn (self, event, flow, packet):
@@ -57,30 +60,39 @@ class Firewall (object):
     handler will be called when the first actual payload data
     comes across the connection.
     """
-    packetHostNameRegex = re.search('Host: ([^\s]+)', packet.payload.payload.payload)
-    if packetHostNameRegex is None:
+
+    host_name_regex = re.search('Host: ([^\s]+)', packet.payload.payload.payload)
+    host_name = host_name_regex.group(0)[6:] if host_name_regex else None
+
+    if self.debug: log.debug("Host: " + host_name)
+    if not host_name:
       if flow.dst in self.monitor_strings.keys():
-        log.debug("Monitored connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
-        event.action.monitor_forward = True
+        if self.debug: log.debug("Monitoring connection " + self.str_conn(flow) + " | Host: " + host_name)
+        event.action.monitor_forward = True #why?
         event.action.monitor_backward = True
-      log.debug("Allowed connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+      if self.debug: log.debug("Allowed connection " + self.str_conn(flow) + " | Host: " + host_name)
       event.action.forward = True
-    packetHostName = packetHostNameRegex.group(0)[6:]
+      return # changed
+    
     for domain in self.banned_domains:
-      if domain == packetHostName:
-        log.debug("Denied connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+      if domain == host_name:
+        if self.debug: log.debug("Denied connection " + self.str_conn(flow) + " | Host: " + host_name)
         event.action.forward = False
         return
-      if len(packetHostName) > len(domain) and packetHostName[(len(packetHostName)-len(domain)):len(packetHostName)] == domain:
-        if packetHostName[len(packetHostName)-len(domain)-1] == '.':
-          log.debug("Denied connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+      hl = len(host_name)
+      dl = len(domain)
+      if hl > dl and host_name[hl - dl : hl] == domain:
+        if host_name[hl - dl - 1] == '.':
+          if self.debug: log.debug("Denied connection " + self.str_conn(flow) + " | Host: " + host_name)
           event.action.forward = False
-          return       
+          return
+
     if flow.dst in self.monitor_strings.keys():
-      log.debug("Monitored connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+      if self.debug: log.debug("Monitoring connection " + self.str_conn(flow) + " | Host: " + host_name)
       event.action.monitor_forward = True
       event.action.monitor_backward = True
-    log.debug("Allowed connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+
+    if self.debug: log.debug("Allowed connection " + self.str_conn(flow) + " | Host: " + host_name)
     event.action.forward = True
     
   def _handle_MonitorData (self, event, packet, reverse):
@@ -89,52 +101,55 @@ class Firewall (object):
     Called when data passes over the connection if monitoring
     has been enabled by a prior event handler.
     """
-    ipAddress = packet.payload.dstip
-    port = packet.payload.payload.dstport
+
+    if ip_address in self.timers.keys():
+      self.timers[ip_address].cancel()
+
+    ip_address = str(packet.payload.dstip)
+    port = str(packet.payload.payload.dstport)
     if reverse:
-      ipAddress = packet.payload.srcip
-      port = packet.payload.payload.srcport
-    httpData = packet.payload.payload.payload
+      ip_address = str(packet.payload.srcip)
+      port = str(packet.payload.payload.srcport)
+    
+    http_data = str(packet.payload.payload.payload)
 
-    if ipAddress in self.timers.keys():
-      self.timers[ipAddress].cancel()
-
-    currentDict = self.monitor_strings[str(ipAddress)]
-    for searchString in currentDict.keys():
-      data = self.prev_packet[str(ipAddress)][searchString] + httpData
-      log.debug("data" + data)
-      self.prev_packet[str(ipAddress)][searchString] = ""
-      searchStringSearchObj = re.findall('%s' % searchString, data)
-      currentDict[searchString] += len(searchStringSearchObj)
-      log.debug("in Monitor: " + str(self.monitor_strings))
-      seenSoFar = ""
-      httpIndex = data.rfind(searchString[0])
-      self.prev_packet[str(ipAddress)][searchString] += searchString[0]
+    for monitor_string in self.monitor_strings[ip_address].keys():
+      data = self.prev_packet[ip_address][monitor_string] + http_data
+      if self.debug: log.debug("data" + data)
+      self.prev_packet[ip_address][monitor_string] = ""
+      monitor_stringSearchObj = re.findall(monitor_string, data)
+      self.monitor_strings[ip_address][monitor_string] += len(monitor_stringSearchObj)
+      if self.debug: log.debug("in Monitor: " + str(self.monitor_strings))
+  
+      httpIndex = data.rfind(monitor_string[0])
+      self.prev_packet[ip_address][monitor_string] += monitor_string[0]
       if httpIndex >= 0:
-        for searchStringIndex in range(1, len(searchString)): #remove -1 from this line since range is not inclusive
-          if httpIndex + searchStringIndex >= len(data):
+        for monitor_stringIndex in range(1, len(monitor_string)): #remove -1 from this line since range is not inclusive
+          if httpIndex + monitor_stringIndex >= len(data):
             break
-          if data[httpIndex+searchStringIndex] == searchString[searchStringIndex] and searchStringIndex != len(searchString)-1: #keep negative 1 here (i'm pretty sure we didn't have it here when we were having problems, right?) because length-1 is last char in string, would explain why things weren't working before, without this it would never realize it found the entire string, so it would save the full word "bing"
-            self.prev_packet[str(ipAddress)][searchString] += searchString[searchStringIndex]
+          if data[httpIndex+monitor_stringIndex] == monitor_string[monitor_stringIndex] and monitor_stringIndex != len(monitor_string)-1: #keep negative 1 here (i'm pretty sure we didn't have it here when we were having problems, right?) because length-1 is last char in string, would explain why things weren't working before, without this it would never realize it found the entire string, so it would save the full word "bing"
+            self.prev_packet[ip_address][monitor_string] += monitor_string[monitor_stringIndex]
           else:
-            self.prev_packet[str(ipAddress)][searchString] = ""
+            self.prev_packet[ip_address][monitor_string] = ""
             break    
 
-      log.debug("timers")
-      self.timers[ipAddress] = Timer(5.0, self.handle_ConnectionClosed, args=(ipAddress, port))
-      log.debug("timer created")
+      self.timers[ip_address] = Timer(30, self.handle_closed_connection, args=(ip_address, port))
+      if self.debug: log.debug("timer created")
 
-  def handle_ConnectionClosed(self, ipAddress, destPort):
-    log.debug("in connectionclos/ed")
-    writeFile = open('/root/pox/ext/counts.txt', 'a')
-    line = str(ipAddress) + "," + str(destPort) + ","
-    log.debug("line: " + line)
-    currentDict = self.monitor_strings[str(ipAddress)]
-    log.debug("currentDict: " + str(currentDict))
-    for searchString in currentDict.keys():
-      val = currentDict[searchString]
-      addLine = line + searchString + "," + str(val) + '\n'
-      log.debug("addline: " + addLine)
-      writeFile.write(addLine)
-      currentDict[searchString] = 0
-      log.debug("I wrote to the file")
+  def handle_closed_connection(self, ip_address, dst_port):
+    ip_address = str(ip_address)
+    dst_port = str(dst_port)
+
+    if self.debug: log.debug("in connectionclos/ed")
+
+    count = ip_address + "," + dst_port + "," + "%s,%s\n"
+
+    if self.debug: log.debug("self.monitor_strings[ip_address]: " + self.monitor_strings[ip_address])
+    for monitor_string in self.monitor_strings[ip_address].keys():
+      self.counts_file.write(count % (monitor_string, self.monitor_strings[ip_address][monitor_string]))
+      counts_file.flush()
+      self.monitor_strings[ip_address][monitor_string] = 0
+    if self.debug: log.debug("I wrote to the file")
+      
+    
+          
